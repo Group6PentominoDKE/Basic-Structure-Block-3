4e95e6f3f1cbdf7c3bb2d1bb84d266f5db8b9dd2
zhecho1215 <zhecho15@yahoo.com>
2018-01-24 02:18:03 +0100

    javaDoc

--- Algorithm.java
+++ Algorithm.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
+/**
+ * @author Zhecho Mitev
+ * Abstract class that contains abstract method solve and other helpful methods for executing every algorithm
+ */
+
+
 public abstract class Algorithm {
 
     public abstract int[][][] solve();
 
-    protected Pentomino[] parcelRotations;
+
+    protected Parcel[] parcelRotations;
 
     protected int width;
     protected int height;
@@ -20,6 +27,14 @@ public abstract class Algorithm {
 
     protected int score;
 
+    /**
+     * Constructor that sets the needed environment for every algorithm
+     * @param width width of the container
+     * @param height height of the container
+     * @param depth depth of the container
+     * @param values values that are going to be assigned to the parcels
+     * @param inputArray character representation of the sorted parcels by value
+     */
     public Algorithm(int width, int height, int depth, ArrayList<Integer> values, char[] inputArray)
     {
         this.width = width;
@@ -32,11 +47,14 @@ public abstract class Algorithm {
         score = 0;
         solutionFound = false;
 
-        parcelRotations = PentominoFactory.createLetters(inputArray);
+        parcelRotations = RotationsFactory.createLetters(inputArray);
         this.values = values;
         assignValues();
     }
 
+    /**
+     * initializing all the cells of the cargo with ' '(space)
+     */
     private void initCargoSpace() {
         cargoSpace = new char[depth][height][width];
         for (int l = 0; l < depth; l++) {
@@ -49,6 +67,9 @@ public abstract class Algorithm {
     }
 
 
+    /**
+     * each parcels receives its value here
+     */
     private void assignValues() {
         for (int i = 0; i < parcelRotations.length; i++) {
             if(parcelRotations[i].getType() == 'a' || parcelRotations[i].getType() == 'l')
@@ -67,33 +88,34 @@ public abstract class Algorithm {
     }
 
     /**
-     * The method gets the shape of the current pentomino, puts it on the cargoSpace and assign the current cell as filled
-     * The method also makes the position of the pentomino in boolean array true, so that it cannot be used again
+     * The method gets the shape of the current parcel, puts it on the cargoSpace and assign the current cell as filled
+     * The method also makes the position of the parcel in boolean array true, so that it cannot be used again
      * @param x coordinate of x axis
      * @param y coordinate of y axis
-     * @param pentomino the current pentomino
+     * @param z coordinate of z axis
+     * @param parcel the current parcel
      */
-    protected void putBlockOnTheBoard(int x, int y, int z, Pentomino pentomino) {
+    protected void putBlockOnTheBoard(int x, int y, int z, Parcel parcel) {
         int xPosition;
         int yPosition;
         int zPosition;
 
-        int planes = pentomino.getPositioningInSpace().length;
-        int rows = pentomino.getPositioningInSpace()[0].length;
-        int cols = pentomino.getPositioningInSpace()[0][0].length;
+        int planes = parcel.getPositioningInSpace().length;
+        int rows = parcel.getPositioningInSpace()[0].length;
+        int cols = parcel.getPositioningInSpace()[0][0].length;
 
-        int startX = pentomino.xIndexOfStartPoint();
-        int startY = pentomino.yIndexOfStartPoint();
-        int startZ = pentomino.zIndexOfStartPoint();
+        int startX = parcel.xIndexOfStartPoint();
+        int startY = parcel.yIndexOfStartPoint();
+        int startZ = parcel.zIndexOfStartPoint();
 
         for (int k = 0; k < planes; k++) {
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
-                    if (pentomino.getPositioningInSpace()[k][i][j] == 1) {
+                    if (parcel.getPositioningInSpace()[k][i][j] == 1) {
                         xPosition = x + k - startX;
                         yPosition = y + i - startY;
                         zPosition = z + j - startZ;
-                        cargoSpace[xPosition][yPosition][zPosition] = pentomino.getType();
+                        cargoSpace[xPosition][yPosition][zPosition] = parcel.getType();
                         boxOfIDs[xPosition][yPosition][zPosition] = ID;
                     }
                 }
@@ -103,28 +125,29 @@ public abstract class Algorithm {
     }
 
     /**
-     * The method gets the shape of the current pentomino and only checks if it could fit on the cargoSpace.
+     * The method gets the shape of the current parcel and only checks if it could fit on the cargoSpace.
      * @param x coordinate of x axis
      * @param y coordinate of y axis
-     * @param pentomino the current pentomino
+     * @param z coordinate of z axis
+     * @param parcel the current parcel
      */
-    protected boolean currentFits(int x, int y, int z, Pentomino pentomino) {
+    protected boolean currentFits(int x, int y, int z, Parcel parcel) {
         int xPosition;
         int yPosition;
         int zPosition;
 
-        int planes = pentomino.getPositioningInSpace().length;
-        int rows = pentomino.getPositioningInSpace()[0].length;
-        int cols = pentomino.getPositioningInSpace()[0][0].length;
+        int planes = parcel.getPositioningInSpace().length;
+        int rows = parcel.getPositioningInSpace()[0].length;
+        int cols = parcel.getPositioningInSpace()[0][0].length;
 
-        int startX = pentomino.xIndexOfStartPoint();
-        int startY = pentomino.yIndexOfStartPoint();
-        int startZ = pentomino.zIndexOfStartPoint();
+        int startX = parcel.xIndexOfStartPoint();
+        int startY = parcel.yIndexOfStartPoint();
+        int startZ = parcel.zIndexOfStartPoint();
 
         for (int k = 0; k < planes; k++) {
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
-                    if (pentomino.getPositioningInSpace()[k][i][j] == 1) {
+                    if (parcel.getPositioningInSpace()[k][i][j] == 1) {
                         xPosition = x + k - startX;
                         yPosition = y + i - startY;
                         zPosition = z + j - startZ;
@@ -144,29 +167,30 @@ public abstract class Algorithm {
     }
 
     /**
-     * The method gets the shape of the current pentomino, removes it from the cargoSpace and assign the current cell as empty(not filled)
-     * The method also makes the position of the pentomino in boolean array false, so that it can be used again
+     * The method gets the shape of the current parcel, removes it from the cargoSpace and assign the current cell as empty(not filled)
+     * The method also makes the position of the parcel in boolean array false, so that it can be used again
      * @param x coordinate of x axis
      * @param y coordinate of y axis
-     * @param pentomino the current pentomino
+     * @param z coordinate of z axis
+     * @param parcel the current parcel
      */
-    protected void removeBlock(int x, int y, int z, Pentomino pentomino) {
+    protected void removeBlock(int x, int y, int z, Parcel parcel) {
         int xPosition;
         int yPosition;
         int zPosition;
 
-        int planes = pentomino.getPositioningInSpace().length;
-        int rows = pentomino.getPositioningInSpace()[0].length;
-        int cols = pentomino.getPositioningInSpace()[0][0].length;
+        int planes = parcel.getPositioningInSpace().length;
+        int rows = parcel.getPositioningInSpace()[0].length;
+        int cols = parcel.getPositioningInSpace()[0][0].length;
 
-        int startX = pentomino.xIndexOfStartPoint();
-        int startY = pentomino.yIndexOfStartPoint();
-        int startZ = pentomino.zIndexOfStartPoint();
+        int startX = parcel.xIndexOfStartPoint();
+        int startY = parcel.yIndexOfStartPoint();
+        int startZ = parcel.zIndexOfStartPoint();
 
         for (int k = 0; k < planes; k++) {
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
-                    if(pentomino.getPositioningInSpace()[k][i][j] == 1) {
+                    if(parcel.getPositioningInSpace()[k][i][j] == 1) {
                         xPosition = x + k - startX;
                         yPosition = y + i - startY;
                         zPosition = z + j - startZ;;
