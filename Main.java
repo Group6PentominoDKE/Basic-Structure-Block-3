0e6bcfb549fb69b40d3185dd23b7591615e7dbaa
zhecho1215 <zhecho15@yahoo.com>
2018-01-24 02:00:43 +0100

    javaDoc + kaspar's sort

--- Main.java
+++ Main.java
@@ -1,118 +1,45 @@
 import java.util.ArrayList;
-import java.util.Scanner;
-import javafx.stage.Stage;
+
+/**
+ * @author Zhecho Mitev
+ * @author Kaspar Kallast
+ * @author Lukas Padolevicius
+ */
 
 public class Main {
 
-    private static Pentomino[] parcelRotations;
-
-    private static int[][][] everyShape;
-
-    private static boolean usingPents = false;
     private static ArrayList<Integer> values = new ArrayList<>();
 
+    /**
+     * The main logic that find the algorithm and executes it
+     * @param userSettings dimensions of the container and values of the boxes
+     * @param pentMode true if TPL, false if ABC
+     * @param algorithmChoice type of the algoirthm
+     * @return box of IDs that would be converted into 3D box using JavFX
+     */
     public int[][][] compute(int[] userSettings, boolean pentMode, String algorithmChoice) {
+        int depth = userSettings[5];
         int width = userSettings[3];
         int height = userSettings[4];
-        int depth = userSettings[5];
 
-        System.out.println("Write letters without spaces:  ");
-        String input = null;
-        if(pentMode == true) {
-          input = "lpt";
+        String input;
+        if(pentMode) {
+            input = "lpt";
         } else {
-          input = "abc";
+            input = "abc";
         }
         char[] inputArray = input.toLowerCase().toCharArray();
         // Make the values array start in descending order(so the 1st value is the highest, 2nd value is 2nd highest and etc.)
-        
-		if (userSettings[0] >= userSettings[1] && userSettings[0] >= userSettings[2]) {
-			/*
-			values.add(userSettings[0]);
-			if (userSettings[1] >= userSettings[2]) {
-				values.add(userSettings[1]);
-				values.add(userSettings[2]);
-			}
-			else {
-				values.add(userSettings[2]);
-				values.add(userSettings[1]);
-			}
-			*/
-			
-				inputArray[0] = input.charAt(0);
-				if (userSettings[1] >= userSettings[2]) {
-					inputArray[1] = input.charAt(1);
-					inputArray[2] = input.charAt(2);
-				}
-				else {
-					inputArray[1] = input.charAt(2);
-					inputArray[2] = input.charAt(1);
-				}
-		}
-		else if (userSettings[1] > userSettings[0] && userSettings[1] >= userSettings[2]) {
-			/*
-			values.add(userSettings[1]);
-			if (userSettings[0] >= userSettings[2]) {
-				values.add(userSettings[0]);
-				values.add(userSettings[2]);
-			}
-			else {
-				values.add(userSettings[2]);
-				values.add(userSettings[0]);
-			}     
-			*/
-		
-				inputArray[0] = input.charAt(1);
-				if (userSettings[0] >= userSettings[2]) {
-					inputArray[1] = input.charAt(0);
-					inputArray[2] = input.charAt(2);
-				}
-				else {
-					inputArray[1] = input.charAt(2);
-					inputArray[2] = input.charAt(0);
-				}
-			}
-		
-		
-		else if (userSettings[2] > userSettings[0] && userSettings[2] > userSettings[1]) {
-			/*
-			values.add(userSettings[2]);
-			if (userSettings[0] >= userSettings[1]) {
-				values.add(userSettings[0]);
-				values.add(userSettings[1]);
-			}
-			else {
-				values.add(userSettings[1]);
-				values.add(userSettings[0]);
-			}
-			*/
-				inputArray[0] = input.charAt(2);
-				if (userSettings[0] >= userSettings[1]) {
-					inputArray[1] = input.charAt(0);
-					inputArray[2] = input.charAt(1);
-				}
-				else {
-					inputArray[1] = input.charAt(1);
-					inputArray[2] = input.charAt(0);
-				}
-			}
-		
-		parcelRotations = PentominoFactory.createLetters(inputArray);
-		
-		
-		
+        sortParcels(userSettings, input, inputArray);
+
         values.add(userSettings[0]);
         values.add(userSettings[1]);
         values.add(userSettings[2]);
-		
-        assignValues();
-           // values.get(0);
-        everyShape = new int[depth][height][width];
+
         long startTime = System.nanoTime();
-        String command = algorithmChoice;
+
         Algorithm algorithm;
-        System.out.println(algorithmChoice);
-        switch (command) {
+        switch (algorithmChoice) {
             case "Greedy":
                 algorithm = new Greedy(width, height, depth, values, inputArray);
                 break;
@@ -130,16 +57,16 @@ public class Main {
                 break;
         }
 
-        everyShape = algorithm.solve();
+        int[][][] bestAnswer = algorithm.solve();
         long endTime = System.nanoTime();
 
         System.out.println("Answer found in " + (endTime - startTime) / 1000000 + " milliseconds");
 
-        if (everyShape != null) {
+        if (bestAnswer != null) {
             for (int i = 0; i < depth; i++) {
                 for (int l = 0; l < height; l++) {
                     for (int j = 0; j < width; j++) {
-                        System.out.print(everyShape[i][l][j] + " ");
+                        System.out.print(bestAnswer[i][l][j] + " ");
                     }
                     System.out.println();
                 }
@@ -147,21 +74,53 @@ public class Main {
             }
             System.out.println(algorithm.score);
         } else {
-            everyShape = new int[1][1][1];
-            everyShape[0][0][0] = 215789;
+            bestAnswer = new int[1][1][1];
+            bestAnswer[0][0][0] = 215789;
             System.out.println("No possible solution");
         }
-        return everyShape;
+        return bestAnswer;
     }
 
-    private static void assignValues() {
-        for (int i = 0; i < parcelRotations.length; i++) {
-            if (parcelRotations[i].getType() == 'a' || parcelRotations[i].getType() == 'l') {
-                parcelRotations[i].setValue(values.get(0));
-            } else if (parcelRotations[i].getType() == 'b' || parcelRotations[i].getType() == 'p') {
-                parcelRotations[i].setValue(values.get(1));
-            } else if (parcelRotations[i].getType() == 'c' || parcelRotations[i].getType() == 't') {
-                parcelRotations[i].setValue(values.get(2));
+    /**
+     * sorting parcels by their value
+     * @param userSettings contains the values of the boxes
+     * @param input the parcels not sorted
+     * @param inputArray array containing the sorted parcels after execution of the method
+     */
+    private void sortParcels(int[] userSettings, String input, char[] inputArray) {
+        if (userSettings[0] >= userSettings[1] && userSettings[0] >= userSettings[2]) {
+            inputArray[0] = input.charAt(0);
+            if (userSettings[1] >= userSettings[2]) {
+                inputArray[1] = input.charAt(1);
+                inputArray[2] = input.charAt(2);
+            }
+            else {
+                inputArray[1] = input.charAt(2);
+                inputArray[2] = input.charAt(1);
+            }
+        }
+        else if (userSettings[1] > userSettings[0] && userSettings[1] >= userSettings[2]) {
+            inputArray[0] = input.charAt(1);
+            if (userSettings[0] >= userSettings[2]) {
+                inputArray[1] = input.charAt(0);
+                inputArray[2] = input.charAt(2);
+            }
+            else {
+                inputArray[1] = input.charAt(2);
+                inputArray[2] = input.charAt(0);
+            }
+        }
+
+
+        else if (userSettings[2] > userSettings[0] && userSettings[2] > userSettings[1]) {
+            inputArray[0] = input.charAt(2);
+            if (userSettings[0] >= userSettings[1]) {
+                inputArray[1] = input.charAt(0);
+                inputArray[2] = input.charAt(1);
+            }
+            else {
+                inputArray[1] = input.charAt(1);
+                inputArray[2] = input.charAt(0);
             }
         }
     }
